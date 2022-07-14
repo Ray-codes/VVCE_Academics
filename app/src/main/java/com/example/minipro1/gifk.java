2e8835523a239d788decce2a866a7f2964082956
mdray <4vv19cs095@gmail.com>
2022-07-14 12:03:36 +0530

    commit

--- app/src/main/java/com/example/minipro1/gifk.java
+++ app/src/main/java/com/example/minipro1/gifk.java
@@ -11,12 +11,19 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class gifk extends AppCompatActivity {
 
     RadioGroup radioGroup;
     RadioButton radioButton;
     EditText editText;
 
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    int rid;
+    String subject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +32,26 @@ public class gifk extends AppCompatActivity {
         radioGroup = findViewById(R.id.Rgroup);
         editText= findViewById(R.id.panju);
 
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+
+
         Button buttonsubmit = findViewById(R.id.FSUBMIT);
         buttonsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String feedback = editText.getText().toString();
+                reference.child("Feedback").child("feedback").child("subject").setValue(subject);
+                reference.child("Feedback").child("feedback").child("feed").setValue(feedback );
             }
         });
 
     }
     public void lallu(View view) {
-        int rid = radioGroup.getCheckedRadioButtonId();
+        rid = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(rid);
+        subject = radioButton.getText().toString();
         Toast.makeText(this,"Successfully Selected " + radioButton.getText(),Toast.LENGTH_SHORT).show();
     }
 }
